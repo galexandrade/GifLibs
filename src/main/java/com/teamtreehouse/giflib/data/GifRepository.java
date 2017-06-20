4aba9411f3008899cc30a256a6b27d7bb0caf23c
Alex Andrade <g.alex.andrade@gmail.com>
2017-06-20 16:30:33 -0300

    Commit

---
+++ src/main/java/com/teamtreehouse/giflib/data/GifRepository.java
@@ -0,0 +1,34 @@
+package com.teamtreehouse.giflib.data;
+
+import com.teamtreehouse.giflib.model.Gif;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class GifRepository {
+    private static final List<Gif> ALL_GIFS = Arrays.asList(
+        new Gif("android-explosion", 1, LocalDate.of(2017, 06, 20), "Alex Andrade", false),
+        new Gif("ben-and-mike", 2, LocalDate.of(2017, 06, 20), "Alex Andrade", true),
+        new Gif("book-dominos", 3, LocalDate.of(2017, 06, 20), "Alex Andrade", true),
+        new Gif("compiler-bot", 2, LocalDate.of(2017, 06, 20), "Alex Andrade", true),
+        new Gif("cowboy-coder", 1, LocalDate.of(2017, 06, 20), "Alex Andrade", true),
+        new Gif("infinite-andrew", 1, LocalDate.of(2017, 06, 20), "Alex Andrade", true),
+        new Gif("robot", 2, LocalDate.of(2017, 06, 20), "Alex Andrade", true)
+    );
+
+    public Gif findByName(String name){
+        for (Gif gif : ALL_GIFS){
+            if (gif.getName().equals(name))
+                return gif;
+        }
+
+        return null;
+    }
+
+    public List<Gif> getAllGifs(){
+        return ALL_GIFS;
+    }
+}
---
+++ src/main/java/com/teamtreehouse/giflib/model/Category.java
@@ -0,0 +1,30 @@
+package com.teamtreehouse.giflib.model;
+
+/**
+ * Created by alex.andrade on 20/06/2017.
+ */
+public class Category {
+    private Integer id;
+    private String name;
+
+    public Category(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
